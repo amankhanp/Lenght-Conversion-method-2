84437831d0297e3fba27d44951c4328cd9e1a1ae
amankhanp <34658950+amankhanp@users.noreply.github.com>
2017-12-21 02:35:33 +0530

    From Meters to Centimeter

---
+++ MeterToCentimeter.java
@@ -0,0 +1,19 @@
+package etoday;
+import java.util.Scanner;
+public class MeterToCentimeter implements LengthConversion{
+	/*
+	 * While implementing an interface first we need to check package,
+	 * whether it is in same package or not.
+	 */
+	public void conversion() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a value in meter : ");
+		float meters = input.nextFloat();
+		float centimeters = meters * 100;
+		System.out.println("meters : " +meters + " = centimeters : " + centimeters);
+	}
+	public static void main(String[] args){
+		MeterToCentimeter mcobject = new MeterToCentimeter();
+		mcobject.conversion();
+	}
+}
