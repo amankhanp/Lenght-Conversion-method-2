5cb708120e64eddfdfcd2a1b2e0c73670ce34c22
amankhanp <34658950+amankhanp@users.noreply.github.com>
2017-12-21 02:34:30 +0530

    From Inch to Meters

---
+++ InchToMeters.java
@@ -0,0 +1,20 @@
+package etoday;
+import java.util.*;
+public class InchToMeters implements LengthConversion{
+	/*
+	 * While implementing an interface first we need to check package,
+	 * whether it is in same package or not.
+	 */
+	public void conversion() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a value in Inch : ");
+		double inch = input.nextDouble();
+		double meters = inch * 0.0254;
+		System.out.println("inch : " +inch + " = meters : " + meters);
+	}
+	public static void main(String[] args){
+		InchToMeters imobject = new InchToMeters();
+		imobject.conversion();
+	}
+}
+
